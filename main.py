b50ccd2af6833b3da730f496ec9b17a6440d6ec5
shaikhkhushi-01 <khushishaikh4860@gmail.com>
2026-03-06 15:42:38 +0530

    fix frontend api format

--- main.py
+++ main.py
@@ -84,7 +84,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",
+        "http://islamic-ai-frontend.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
