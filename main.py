e6a3e07e8e2015b504b6b9c5134246ecc9f2b6eb
shaikhkhushi-01 <khushishaikh4860@gmail.com>
2026-03-06 16:27:03 +0530

    add history route

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
-        "http://islamic-ai-frontend.vercel.app"
+        "https://islamic-ai-frontend.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
