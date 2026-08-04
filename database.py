30eb507b88367b1abf5e2a2878e80949d3747d9c
Khushi Shaikh <khushishaikh4860@gmail.com>
2026-08-04 13:04:11 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,74 @@
+import sqlite3
+import os
+from dotenv import load_dotenv
+from passlib.context import CryptContext
 
+load_dotenv()
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "islamic_ai.db")
+
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
+
+
+def get_connection():
+    return sqlite3.connect(DB_PATH)
+
+
+def init_db():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS knowledge(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        topic TEXT,
+        content TEXT,
+        type TEXT,
+        detailed_content TEXT,
+        reference TEXT
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        email TEXT UNIQUE,
+        password TEXT,
+        role TEXT DEFAULT 'user'
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS chat_history(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        question TEXT,
+        answer TEXT,
+        intent TEXT,
+        created_at TEXT
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS chat_memory(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        session_id TEXT,
+        last_topic TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+
+def hash_password(password):
+    return pwd_context.hash(password)
+
+
+def verify_password(password, hashed):
+    return pwd_context.verify(password, hashed)
